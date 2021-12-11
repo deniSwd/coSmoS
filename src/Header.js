b7bcd0549d87fcaf1f9929b5d0e9f440aac4a92f
deniSwd <denis_swd@mail.ru>
2021-12-11 19:29:33 +0300

    first commit eeeee...

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <a>Home</a>
+            <a>Main</a>
+            <a>News</a>
+            <a>Contact</a>
+        </div>
+    );
+}
+export default Header;
