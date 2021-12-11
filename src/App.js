889dc19b84ce2da7fa030470ae4346d4ac351300
deniSwd <denis_swd@mail.ru>
2021-12-11 23:42:16 +0300

    added main blocks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
+import logoimg from './assets/my images/logo.png'
+import contentimg from './assets/my images/content.jpg'
+import userimg from './assets/my images/user.jpg'
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src={logoimg}/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a href='#s'>Profile</a>
+                </div>
+                <div>
+                    <a>Masseges</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settins</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div className='content-img'>
+                    <img src={contentimg}/>
+                </div>
+                <div>
+                    <img src={userimg}/>
+                </div>
+                <div>
+                    My Posts
+                </div>
+                <div>
+                    New Posts
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
