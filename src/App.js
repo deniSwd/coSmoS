b16813c4b5b1d4aeadfaa231f4a70735a6ee6bca
deniSwd <denis_swd@mail.ru>
2021-12-12 18:29:50 +0300

    deniSwd 12/12/2021 added components header navbar profile

--- src/App.js
+++ src/App.js
@@ -3,51 +3,16 @@ import './App.css';
 import logoimg from './assets/my images/logo.png'
 import contentimg from './assets/my images/content.jpg'
 import userimg from './assets/my images/user.jpg'
+import Header from './Components/Header'
+import Navbar from "./Components/Navbar";
+import Profile from "./Components/Profile";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img src={logoimg}/>
-            </header>
-            <nav className='nav'>
-                <div>
-                    <a href='#s'>Profile</a>
-                </div>
-                <div>
-                    <a>Masseges</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settins</a>
-                </div>
-            </nav>
-            <div className='content'>
-                <div className='content-img'>
-                    <img src={contentimg}/>
-                </div>
-                <div>
-                    <img src={userimg}/>
-                </div>
-                <div>
-                    My Posts
-                </div>
-                <div>
-                    New Posts
-                    <div>
-                        Post 1
-                    </div>
-                    <div>
-                        Post 2
-                    </div>
-                </div>
-
-            </div>
+         <Header/>
+          <Navbar/>
+            <Profile/>
         </div>
     );
 }
