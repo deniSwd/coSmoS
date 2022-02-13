c3ac4736de92dbc03ac5456faa52045f8db69b2c
deniSwd <denis_swd@mail.ru>
2022-02-13 22:14:15 +0300

    deniSwd 13/02/2022 install redux form and setting my login form

--- src/Components/Login/login.jsx
+++ src/Components/Login/login.jsx
@@ -1,9 +1,35 @@
+import style from './login.Module.css'
+import {Field, reduxForm} from "redux-form";
 
-const Login = (props) => {
+const LoginForm = (props) => {
     return (
-        <h1>
-            LOGIN
-        </h1>
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field component={'input'} name={'login'} placeholder={'Login'}/>
+            </div>
+            <div>
+                <Field component={'input'} name={'password'} placeholder={'Password'}/>
+            </div>
+            <div>
+                <Field component={'input'}  name={'rememberMe'} type={"checkbox"}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
+const Login = (props) => {
+    const onSubmit =(formData)=> {
+        console.log(formData)
+    }
+    return (<div>
+            <div className={style.login}>
+                <h1>LOGIN</h1>
+                <LoginReduxForm onSubmit={onSubmit}/>
+            </div>
+        </div>
     )
 }
 export default Login;
