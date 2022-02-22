563d0cb214e1dbefe4b337c2a9a77c62db862505
deniSwd <denis_swd@mail.ru>
2022-02-22 19:47:13 +0300

    deniSwd 22/02/2022 improved Paginator

--- src/Components/Common/ObjectHelpers/Paginator.jsx
+++ src/Components/Common/ObjectHelpers/Paginator.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from "./Paginator.module.css";
 
 
-const Paginator = ({totalUsersCount, pageSize, currentPage, onPostChanged}) => {
-    let pageCount = Math.ceil(totalUsersCount / pageSize)
+const Paginator = ({totalItemsCount, pageSize, currentPage, onPostChanged, fractionSize = 10}) => {
+    let pageCount = Math.ceil(totalItemsCount / pageSize)
     let page = []
     for (let i = 1; i <= pageCount; i++) {
         page.push(i)
     }
+
+    let fractionCount = Math.ceil(pageCount / fractionSize)
+    let [fractionNumber, setFractionNumber] = useState(1)
+    let leftElementFraction = ((fractionNumber - 1) * fractionSize)+1
+    let rightElementFraction = fractionNumber * fractionSize
+
     return (
         <div className={style.content}>
-            {page.map(p => {
-                return <span className={currentPage === p && style.selectedPage}
-                             onClick={(event) => {
-                                 onPostChanged(p)
-                             }}>{p}</span>
-            })}
+            {fractionNumber > 1 &&
+            <button onClick={() => {
+                setFractionNumber(fractionNumber - 1)
+            }}>{'<...'}</button>}
+
+            {page
+                .filter(p => p >= leftElementFraction && p <= rightElementFraction)
+                .map(p => {
+                    return <span className={currentPage === p ? style.selectedPage : style.noSelectedPage }
+                                 onClick={(event) => {
+                                     onPostChanged(p)
+                                 }}>{p}</span>
+                })}
+            {fractionNumber < fractionCount &&
+            <button onClick={() => {
+                setFractionNumber(fractionNumber + 1)
+            }}>{'...>'}</button>}
+
         </div>
     )
 }
--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import Paginator from "../Common/ObjectHelpers/Paginator";
 import User from "./User";
-
+import style from './Users.module.css'
 
 const Users = ({totalUsersCount, pageSize, currentPage, onPostChanged, ...props}) => {
     return (
-        <div>
-            <Paginator totalUsersCount={totalUsersCount} pageSize={pageSize}
-                       currentPage={currentPage} onPostChanged={onPostChanged}/>
+        <div className={style.usersBlock}>
             {props.users.map(u => <User  user ={u}
                                          followingInProgress={props.followingInProgress}
                                          unfollow={props.unfollow}
                                          follow={props.follow}
                                          key={u.id}/>)}
-
+            <Paginator totalItemsCount={totalUsersCount} pageSize={pageSize}
+                       currentPage={currentPage} onPostChanged={onPostChanged}/>
         </div>
     )
 }
